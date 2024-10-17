046d52bb5d5fc9af734f2a5c8e32d5c9ae56ad2b
me <me@dev.local>
2024-10-16 21:40:10 -0700

    main function now initializes the adapters for the service

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use orderbook_trial_task;
+use std::sync::{Arc, Mutex};
 
-fn main() {
-    println!("Hello, world!");
+use orderbook_trial_task::{
+    self,
+    adapters::{Binance, ClientWebServer},
+    application::Application,
+    ports::{WebServer, WebServerSettings},
+};
+
+// Application and needed adapters will be instantiated here
+#[tokio::main]
+async fn main() {
+    let market_api = Arc::new(Mutex::new(Binance {
+        api_endpoint: "".into(),
+        api_key: "".into(),
+        api_secret: "".into(),
+    }));
+    let app_layer = Arc::new(Mutex::new(Application { market_api }));
+
+    let web_server_settings = WebServerSettings {
+        port: "3000".into(),
+    };
+
+    let _ = ClientWebServer::new(web_server_settings, app_layer.clone())
+        .run_server()
+        .await;
 }
