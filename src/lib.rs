b905d33d2cd34f0183d5e7beb7e7690658840864
me <me@dev.local>
2024-10-16 21:53:31 -0700

    Ports, adapters and application modules now public

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+/*
+  Service is made of
+
+  - adapters: contains all the implementations of ports
+  - ports: contains interfaces to be used within the service
+  - application: contains all application layer code
+  - core: contains all pure business logic of domain
+  - typespec: globally available types
+*/
+
+pub mod adapters;
 pub mod application;
+mod core;
+pub mod ports;
+mod typespec;
