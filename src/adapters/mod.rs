c3f4a4ec6e0d493adc6a3e4da13c1662a760baa8
me <me@dev.local>
2024-10-16 22:06:08 -0700

    Adapters now moved to seperate submodules

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -1,21 +1,5 @@
-use std::sync::Arc;
+mod binance_market_stream;
+mod client_web_server;
 
-use crate::ports::MarketStream;
-use crate::typespec::Symbol;
-
-// A infrastructure struct that implements a driven port to be used in
-// the application layer
-pub struct Binance {
-    api_endpoint: String,
-    api_key: String,
-    api_secret: String,
-}
-
-// Uses binance_spot_connector_rust crate to maintain socket connection
-
-impl MarketStream for Binance {
-    fn connect_to_stream(self) -> Self {}
-
-    fn get_order_book_asks(symbol: Symbol) -> Vec<f32> {}
-    fn get_order_book_bids(symbol: Symbol) -> Vec<f32> {}
-}
+pub use binance_market_stream::Binance;
+pub use client_web_server::ClientWebServer;
