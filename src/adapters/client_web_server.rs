34e379f0768d003d8f7b9afd6bc5b0b9abbc9ba7
me <me@dev.local>
2024-10-18 10:40:33 -0700

    Server can now push data via websocket

--- src/adapters/client_web_server.rs
+++ src/adapters/client_web_server.rs
@@ -1,20 +1,21 @@
+use crate::{
+    application::{ApplicationQuery, ApplicationResponse},
+    ports::{WebServer, WebServerSettings},
+    typespec::ApplicationLayer,
+};
 use anyhow::{Error, Result};
-use futures_util::{SinkExt, StreamExt};
+use futures_util::{SinkExt, StreamExt, TryStreamExt};
 use poem::{
-    endpoint::{StaticFileEndpoint, StaticFilesEndpoint},
+    endpoint::StaticFilesEndpoint,
     get, handler,
-    listener::TcpListener,
+    listener::{Listener, TcpListener},
     web::{
-        websocket::{Message, WebSocket},
+        websocket::{CloseCode, Message, WebSocket},
         Data,
     },
     EndpointExt, IntoResponse, Route, Server,
 };
-
-use crate::{
-    ports::{WebServer, WebServerSettings},
-    typespec::ApplicationLayer,
-};
+use serde::{Deserialize, Serialize};
 
 pub struct ClientWebServer {
     settings: WebServerSettings,
@@ -36,19 +37,39 @@ impl WebServer for ClientWebServer {
                 StaticFilesEndpoint::new("frontend/svelte-client/dist").index_file("index.html"),
             )
             .at(
-                "/api/get_average_orderbook_price",
+                "/api/average_order_book_price",
                 get(average_price_web_socket),
             )
             .data(self.app_layer.clone());
 
-        Server::new(TcpListener::bind(format!(
-            "localhost:{}",
-            &self.settings.port
-        )))
-        .run(web_app)
-        .await
-        .map_err(|e| Error::msg(e))
+        let acceptor = TcpListener::bind(format!("localhost:{}", &self.settings.port))
+            .into_acceptor()
+            .await
+            .unwrap();
+
+        Server::new_with_acceptor(acceptor)
+            .run(web_app)
+            .await
+            .map_err(|e| Error::msg(e))
     }
+}
+
+// DTOs
+type Pair = String;
+type Value<'a> = &'a str;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct PairQuery {
+    #[serde(rename(serialize = "p", deserialize = "p"))]
+    pair: Pair,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct PairValue<'v> {
+    #[serde(rename(serialize = "p", deserialize = "p"))]
+    pair: Pair,
+    #[serde(rename(serialize = "v", deserialize = "v"))]
+    value: &'v str,
 }
 
 // Controllers
@@ -59,9 +80,63 @@ async fn average_price_web_socket(
     ws: WebSocket,
     Data(app_layer): Data<&ApplicationLayer>,
 ) -> impl IntoResponse {
+    // clones pointer within function to avoid compile time errors
+    let app_layer = app_layer.clone();
+
     ws.on_upgrade(|mut socket| async move {
-        if let Some(Ok(Message::Text(msg))) = socket.next().await {
-            socket.send(Message::Text(msg)).await;
+        loop {
+            match socket.try_next().await {
+                Ok(Some(Message::Text(msg))) => {
+                    let pair = serde_json::from_str::<PairQuery>(msg.clone().as_str());
+
+                    if let Ok(dto) = pair {
+                        let app_layer_res = app_layer.clone().handle_query(
+                            ApplicationQuery::GetAverageValueOfSymbol(crate::typespec::Symbol(
+                                dto.pair.clone(),
+                            )),
+                        );
+
+                        match app_layer_res {
+                            Ok(ApplicationResponse::CurrentAveragePriceForSymbol {
+                                symbol,
+                                price,
+                            }) => {
+                                //serialize value and return message to client
+                                let pv = PairValue {
+                                    pair: symbol.0,
+                                    value: price.as_str(),
+                                };
+                                let json_res = serde_json::to_string(&pv).unwrap();
+                                let res = Message::text(json_res);
+                                let _ = socket.send(res).await;
+                            }
+                            _ => {
+                                let close_message =
+                                    Message::close_with(CloseCode::Error, "Internal server error");
+                                let _ = socket.send(close_message).await;
+                                break;
+                            }
+                        }
+                    }
+                }
+                Ok(Some(_)) => {
+                    let close_message =
+                        Message::close_with(CloseCode::Unsupported, "unsupported message type");
+                    let _ = socket.send(close_message).await;
+                    break;
+                }
+                Ok(None) => {
+                    let close_message = Message::close_with(CloseCode::Normal, "connection killed");
+                    let _ = socket.send(close_message).await;
+                    break;
+                }
+                Err(e) => {
+                    eprintln!("error: {}", e);
+                    let close_message = Message::close_with(CloseCode::Error, "error with socket");
+                    let _ = socket.send(close_message).await;
+                    break;
+                }
+            }
         }
     })
 }
